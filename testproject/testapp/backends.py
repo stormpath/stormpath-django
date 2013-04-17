4e117c5c514d4b42c819767986f48017c74bf2ee
gcetusic <goran.cetusic@gmail.com>
2013-04-17 16:29:38 +0200

    minor refactor

--- testproject/testapp/backends.py
+++ testproject/testapp/backends.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.db.models import Q
-from django.core.validators import email_re
 from stormpath.client import ClientApplicationBuilder
 from stormpath.resource import ResourceError, enabled
 from stormpath.auth import UsernamePasswordRequest
@@ -11,6 +10,34 @@ class StormpathBackend(object):
     """
     Authenticate against the settings STORMPATH_URL
     """
+
+    def save_user(self, user, account):
+
+        save = False
+        if (user.username != account.username):
+            save = True
+            user.username = account.username
+
+        if (user.email != account.email):
+            save = True
+            user.email = account.email
+
+        if (user.first_name != account.given_name):
+            save = True
+            user.first_name = account.given_name
+
+        if (user.last_name != account.surname):
+            save = True
+            user.last_name = account.surname
+
+        if (user.is_active != (account.status == enabled)):
+            save = True
+            user.is_active = (account.status == enabled)
+
+        if save:
+            user.save()
+
+        return user
 
     def authenticate(self, username=None, password=None):
         href = settings.STORMPATH_URL
@@ -28,40 +55,10 @@ class StormpathBackend(object):
         try:
             user = user_model.objects.get(
                 Q(username=account.username) | Q(email=account.email))
-
-            save = False
-            if not user.username == account.username:
-                save = True
-                user.username = account.username
-
-            if not user.email == account.email:
-                save = True
-                user.email = account.email
-
-            if not user.first_name == account.given_name:
-                save = True
-                user.first_name = account.given_name
-
-            if not user.last_name == account.surname:
-                save = True
-                user.last_name = account.surname
-
-            if not (user.is_active == (account.status == enabled)):
-                save = True
-                user.is_active = (account.status == enabled)
-
-            if save:
-                user.save()
-
         except user_model.DoesNotExist:
-            user = user_model(
-                username=account.username, email=account.email,
-                first_name=account.given_name, last_name=account.surname,
-                password="")
-            user.is_active = (account.status == enabled)
-            user.save()
+            user = user_model(password="STORMPATH")
 
-        return user
+        return self.save_user(user, account)
 
     def get_user(self, user_id):
         try:
