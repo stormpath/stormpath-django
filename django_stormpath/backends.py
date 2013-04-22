ce79bfed3c9f815a1885b3ffa993b1311b30496a
gcetusic <goran.cetusic@gmail.com>
2013-04-22 14:23:47 +0200

    docstrings

--- django_stormpath/backends.py
+++ django_stormpath/backends.py
@@ -9,10 +9,21 @@ from stormpath.auth import UsernamePasswordRequest
 class StormpathBackend(object):
     """
     Authenticate against the settings STORMPATH_URL
+
+    The methods are authenticate and get_user, all Django auth backends
+    are required them and return a user model object for a successful authentication
+    or None otherwise. save_user is a helper function to determine if the user object
+    should be saved.
     """
 
     def save_user(self, user, account):
+        """
+        Save user fields if Stormpath account fields have changed.
 
+        Arguments:
+        user -- user model object
+        account -- Stormpath account object as returned by application.authenticate_account
+        """
         save = False
         if user.username != account.username:
             save = True
@@ -40,6 +51,17 @@ class StormpathBackend(object):
         return user
 
     def authenticate(self, username=None, password=None):
+        """
+        Create a new user model if it doesn't already doesn't already exist or
+        return update and existing user to match the Stormpath account.
+
+        The authenticate method takes credentials as keyword arguments.
+        Usually, the method is used with a username and password as arguments.
+
+        Returns a user model if the Stormpath authentication was successful or
+        None otherwise. It expects a variable STORMPATH_URL to be defined in Django settings:
+        STORMPATH_URL = "https://KeyId:KeySecret@api.stormpath.com/v1/applications/APP_UID"
+        """
         href = settings.STORMPATH_URL
         client_application = ClientApplicationBuilder().set_application_href(href).build()
         application = client_application.application
@@ -61,6 +83,10 @@ class StormpathBackend(object):
         return self.save_user(user, account)
 
     def get_user(self, user_id):
+        """
+        The method takes a user_id - which could be a username, database ID etc.
+        and returns a user model object.
+        """
         try:
             user_model = get_user_model()
             return user_model.objects.get(pk=user_id)
