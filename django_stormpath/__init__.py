10dd30d3a531f82b87e5164b24187765d6b42568
Randall Degges <r@rdegges.com>
2014-12-09 11:20:36 -0800

    Version bump.

--- django_stormpath/__init__.py
+++ django_stormpath/__init__.py
@@ -1,4 +1,4 @@
-__version_info__ = ('0', '0', '2')
+__version_info__ = ('0', '0', '3')
 __version__ = '.'.join(__version_info__)
 __short_version__ = '.'.join(__version_info__)
 __author__ = 'Stormpath, Inc.'
