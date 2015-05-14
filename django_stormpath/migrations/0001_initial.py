6a03864cebd6d35e9a3fffd44fa1fc1de8805f5a
Ana Vojnovic <ana.vojnovic@dobarkod.hr>
2015-05-14 11:37:21 +0200

    Added migrations for all supported Django versions.
    We need south_migrations directory with south migrations for Django 1.6 (https://docs.djangoproject.com/en/1.8/topics/migrations/#libraries-third-party-apps).
    Also, for Django 1.7 and Django 1.8, we have to use migrations generated on Django 1.7 version (https://docs.djangoproject.com/en/1.8/topics/migrations/#supporting-multiple-django-versions). It should work on Django 1.8 (as stated in docs), but it doesn't - the last_login field was made nullable in 1.8. I changed that field in the migration, so it will work for both versions.

--- django_stormpath/migrations/0001_initial.py
+++ django_stormpath/migrations/0001_initial.py
@@ -2,12 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import django.utils.timezone
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('auth', '0006_require_contenttypes_0002'),
+        ('auth', '0001_initial'),
     ]
 
     operations = [
@@ -16,22 +17,23 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('password', models.CharField(max_length=128, verbose_name='password')),
-                ('last_login', models.DateTimeField(null=True, verbose_name='last login', blank=True)),
+                ('last_login', models.DateTimeField(default=django.utils.timezone.now, verbose_name='last login', null=True, blank=True)),
                 ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
                 ('href', models.CharField(max_length=255, null=True, blank=True)),
                 ('username', models.CharField(unique=True, max_length=255)),
                 ('given_name', models.CharField(max_length=255)),
                 ('surname', models.CharField(max_length=255)),
                 ('middle_name', models.CharField(max_length=255, null=True, blank=True)),
-                ('email', models.EmailField(unique=True, max_length=255, verbose_name='email address', db_index=True)),
+                ('email', models.EmailField(unique=True, max_length=255, verbose_name=b'email address', db_index=True)),
                 ('is_active', models.BooleanField(default=True)),
                 ('is_admin', models.BooleanField(default=False)),
                 ('is_staff', models.BooleanField(default=False)),
-                ('groups', models.ManyToManyField(related_query_name='user', related_name='user_set', to='auth.Group', blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', verbose_name='groups')),
+                ('groups', models.ManyToManyField(related_query_name='user', related_name='user_set', to='auth.Group', blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of his/her group.', verbose_name='groups')),
                 ('user_permissions', models.ManyToManyField(related_query_name='user', related_name='user_set', to='auth.Permission', blank=True, help_text='Specific permissions for this user.', verbose_name='user permissions')),
             ],
             options={
                 'abstract': False,
             },
+            bases=(models.Model,),
         ),
     ]
