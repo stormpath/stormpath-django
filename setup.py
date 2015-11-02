e73e612a34c2b80608f33701e435354e0e8a6e4e
Randall Degges <r@rdegges.com>
2015-11-02 14:43:10 -0800

    Version bump for stormpath dependency.

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
     packages = find_packages(),
     install_requires = [
         'requests-oauthlib>=0.4.2',
-        'stormpath>=2.0.7',
+        'stormpath>=2.1.3',
         'Django>=1.6',
     ],
     cmdclass = {
